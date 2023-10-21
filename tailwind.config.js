ed3c4b9ecfee9078d3d53560d0b82572924ab9f5
masa-1013 <wktkmrnhrtmmshr@gmail.com>
2023-10-21 12:53:01 +0900

    tailwind css を導入

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './*.vue',
+    './components/**/*.vue',
+    './layouts/**/*.vue',
+    './pages/**/*.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
