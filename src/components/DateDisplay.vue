19a61134fd066908eedc4b67b0bbdd88156d6234
masa-1013 <wktkmrnhrtmmshr@gmail.com>
2023-10-28 17:22:20 +0900

    日付と日時を表示するコンポーネントの作成

---
+++ src/components/DateDisplay.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    days: number[]
+  }>()
+</script>
+<template>
+  <v-container>
+    <v-row>
+      <template v-for="day in props.days" :key="day">
+        <v-col class="text-center">{{ day }}</v-col>
+      </template>
+    </v-row>
+  </v-container>
+</template>
---
+++ src/components/TimeDisplay.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  const hours = Array.from({ length: 24 }, (_, i) => i)
+
+  const change24HourToAmPm = (hour: number) => {
+    if (hour < 12) {
+      return `午前${hour}時`
+    }
+    // 12時は午後12時と表示する
+    if (hour === 12) {
+      return `午後${hour}時`
+    }
+
+    return `午後${hour - 12}時`
+  }
+</script>
+<template>
+  <div class="w-24"></div>
+  <template v-for="hour in hours" :key="hour">
+    <div class="h-16 text-xs text-right">
+      {{ change24HourToAmPm(hour) }}
+    </div>
+  </template>
+</template>
