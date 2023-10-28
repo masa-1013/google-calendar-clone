e503ae046ab8d776209b8d2abe4b87314e44eb60
masa-1013 <wktkmrnhrtmmshr@gmail.com>
2023-10-28 17:16:44 +0900

    vuetifyのpluginの追加時に発生しているeslintのエラーを無効化

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
   modules: [
     (_options, nuxt) => {
       nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-expect-error
         config.plugins.push(vuetify({ autoImport: true }))
       })
